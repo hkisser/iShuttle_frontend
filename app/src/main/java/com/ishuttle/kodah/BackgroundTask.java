cdc1ff121111de73ae277a39ce65229038a439c3
Obinna <godspoweropara37@gmail.com>
2018-04-03 19:54:48 +0100

    Driver Location Tracking system implemented but not efficient enough

--- app/src/main/java/com/ishuttle/kodah/BackgroundTask.java
+++ app/src/main/java/com/ishuttle/kodah/BackgroundTask.java
@@ -27,6 +27,7 @@ public class BackgroundTask extends AsyncTask<String,Void,String>{
     @Override
     protected String doInBackground(String... params) {
         String reg_url="http://kodahinc.000webhostapp.com/register.php";
+        String geo_url="http://kodahinc.000webhostapp.com/driverlocation.php";
 
         String method=params[0];
         if(method.equals("register")){
@@ -57,6 +58,37 @@ public class BackgroundTask extends AsyncTask<String,Void,String>{
 
 
         }
+        if(method.equals("geostore")){
+
+            String Id=params[1];
+            String route=params[2];
+            String lat_geo=params[3];
+            String lng_geo=params[4];
+            try {
+                URL url=new URL(geo_url);
+                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream os=httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+                String data = URLEncoder.encode("driver_id","UTF-8")+"="+URLEncoder.encode(Id,"UTF-8")+"&"+
+                        URLEncoder.encode("drivers_route","UTF-8")+"="+URLEncoder.encode(route,"UTF-8")+"&"+
+                        URLEncoder.encode("geolat","UTF-8")+"="+URLEncoder.encode(lat_geo,"UTF-8")+"&"+
+                        URLEncoder.encode("geolng","UTF-8")+"="+URLEncoder.encode(lng_geo,"UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                os.close();
+                InputStream IS=httpURLConnection.getInputStream();
+                IS.close();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         return null;
     }
@@ -73,6 +105,7 @@ public class BackgroundTask extends AsyncTask<String,Void,String>{
 
     @Override
     protected void onPostExecute(String result) {
+
         Toast.makeText(ctx,result,Toast.LENGTH_SHORT).show();
     }
 }
